47fdc803d29de1775ed7129406de56b0027437d9
John A. Stevens <john@otherskylines.com>
2022-08-12 07:03:38 -0600

    Programmatically generate chessboard

--- src/frontend/web/chess/src/index.ts
+++ src/frontend/web/chess/src/index.ts
@@ -1,8 +1,40 @@
 import './style.css';
-import './resources/images/Chess_nlt45.svg'
 
-console.log("Script works!")
+const chessboardElement = document.getElementById('chessboard');
 
-const element = document.getElementById('test');
-element.innerText = 'script works!';
-element.style.background = 'blue';
+let chessboard: HTMLDivElement[][] =[];
+
+for (let rowSet = 0; rowSet < 4; ++rowSet) {
+    // do rows in pairs of two
+
+    // row starting with black
+    let row: HTMLDivElement[] = [];
+    for (let col = 0; col < 8; ++col) {
+        const square = document.createElement('div') as HTMLDivElement;
+        square.style.width = '120px';
+        square.style.height = '120px';
+        square.style.background = col % 2 == 0 ? 'white' : 'black';
+        chessboardElement.append(square);
+        row.push(square);
+    }
+    chessboard.push(row);
+
+    // row starting with white
+    row = [];
+    for (let col = 0; col < 8; ++col) {
+        const square = document.createElement('div') as HTMLDivElement;
+        square.style.width = '120px';
+        square.style.height = '120px';
+        square.style.background = col % 2 == 1 ? 'white' : 'black';
+        chessboardElement.append(square);
+        row.push(square);
+    }
+    chessboard.push(row);
+}
+
+const knightImage = document.createElement('img') as HTMLImageElement;
+knightImage.src = 'assets/images/Chess_nlt45.svg';
+knightImage.style.height = '100%';
+knightImage.style.width = '100%';
+
+chessboard[2][3].append(knightImage);
