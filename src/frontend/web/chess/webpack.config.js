14f050bc1738aabec7ee55556bbbd54654db873c
John A. Stevens <john@otherskylines.com>
2022-08-12 04:20:16 -0600

    Add webpack-dev-server

--- src/frontend/web/chess/webpack.config.js
+++ src/frontend/web/chess/webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     module: {
       rules: [
           {
